8a437fe5cfbdbe443e81ab4b8b36b5a551c25bac
krzysztofzielinskicom <133945130+krzysztofzielinskicom@users.noreply.github.com>
2023-07-28 18:51:24 +0200

    Update main.py
    add comments(descriptions about game) to file main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 import sys
-xo="O"
-ilosc_ruchow = 0
-list_cordinates=[[" "," "," "],[" "," "," "],[" "," "," "]]
-def add_to_list(xy,xo):
-
+# liste and values for start the games,
+xo="O" #value who is playing now X or Y - standard O start
+ilosc_ruchow = 0 # value of steps , strt 0 later stop on 8 because can only 9 steps in liste
+list_cordinates=[[" "," "," "],[" "," "," "],[" "," "," "]] #liste 2D of games, here save chosed filed of X or O
+def add_to_list(xy,xo): # add item X or O for filed , check if filed is empty or eary used
     x=int(xy[0])
     y=int(xy[1])
     if list_cordinates[x][y]==" ":
@@ -16,11 +16,12 @@ def add_to_list(xy,xo):
 
 
 
-def check_result_game(list_cordinates,xo):
+def check_result_game(list_cordinates,xo): 
+    #check everyone possible if a player win
     if xo==list_cordinates[0][0]==list_cordinates[0][1]==list_cordinates[0][2] or xo==list_cordinates[1][0]==list_cordinates[1][1]==list_cordinates[1][2] or xo==list_cordinates[2][0]==list_cordinates[2][1]==list_cordinates[2][2] or xo==list_cordinates[0][0]==list_cordinates[1][0]==list_cordinates[2][0] or xo==list_cordinates[0][1]==list_cordinates[1][1]==list_cordinates[2][1] or xo==list_cordinates[0][2]==list_cordinates[1][2]==list_cordinates[2][2] or xo==list_cordinates[0][0]==list_cordinates[1][1]==list_cordinates[2][2] or xo==list_cordinates[0][2]==list_cordinates[1][1]==list_cordinates[2][0]:
         print(f"win {xo}")
         sys.exit(0)
-def print_result_game():
+def print_result_game(): #print result of array items fom list_cordinatins
     #os.system('clear')
     print("---0---1---2--X")
     print("0|",list_cordinates[0][0],"|",list_cordinates[0][1],"|",list_cordinates[0][2],"|")
@@ -29,12 +30,12 @@ def print_result_game():
     print("--------------")
     print("2|",list_cordinates[2][0],"|",list_cordinates[2][1],"|",list_cordinates[2][2],"|")
     print("Y-------------")
-def main_game(xo,ilosc_ruchow,list_cordinates):
+def main_game(xo,ilosc_ruchow,list_cordinates):# main cod  for games and use def add items , check result and print game
     list_cordinates=list_cordinates
     ilosc_ruchow=ilosc_ruchow
     print_result_game()
     while ilosc_ruchow < 9 :
-        print(f'Który ruch jest wykonywany: {ilosc_ruchow +1}')
+        print(f'Wich step is doing: {ilosc_ruchow +1}')
         if xo == "O":
             xy = input("Enter O your xy pole: ")
             if xy[0]== "0" or xy[0]== "1" or xy[0]== "2" :
@@ -70,6 +71,6 @@ def main_game(xo,ilosc_ruchow,list_cordinates):
             else:
                 print("Wrong input X")
                 main_game(xo, ilosc_ruchow, list_cordinates)
-    print("jeszcze nikt nie wygrał")
+    print("X and O not win")
     sys.exit()
 main_game(xo,ilosc_ruchow,list_cordinates)
